c9422b9f3ff2ca53d79e3c50f47beb97254e0817
chno <cnordvaller@gmail.com>
2015-08-25 14:19:08 +0200

    Cleanup

--- app-BBO/front/cnadm/pages/addNewEventForm.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="logic/logic.php?action=addEvent">
-	Event name: <input type="text" name="eventName" /><br />
-	Event date: <input type="text" name="eventDate" value="<?php echo date("Y-m-d"); ?>" /><br />
-    Visible on front page: <input type="checkbox" name="eventDisplay" checked /><br /><br />
-    <input type="submit" value="Add event" />
-</form>
