4b61c0804facc3f3c710c670c90db3992a7700e8
Christian Nordvaller <cnordvaller@gmail.com>
2016-01-03 17:58:45 +0100

    DB engine update and minor fix for email field

--- db/scripts/create_bets_db.sql
+++ db/scripts/create_bets_db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `bets` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 CREATE TABLE  `bets`.`alerts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
   `fight_id` int(10) unsigned NOT NULL DEFAULT '0',
   `fighter` int(10) unsigned NOT NULL DEFAULT '0',
   `bookie_id` int(11) NOT NULL DEFAULT '0',
@@ -19,7 +19,7 @@ CREATE TABLE  `bets`.`bookies` (
   `active` tinyint(1) NOT NULL default '1',
   `position` int(10) unsigned NOT NULL default '2',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`events` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -27,20 +27,20 @@ CREATE TABLE  `bets`.`events` (
   `name` varchar(200) NOT NULL default '',
   `display` tinyint(1) NOT NULL default '1',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`fighters` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(85) NOT NULL default '',
   `url` text NOT NULL,
   PRIMARY KEY  USING BTREE (`id`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`fighters_altnames` (
   `fighter_id` int(11) DEFAULT NULL,
   `altname` varchar(100) DEFAULT NULL,
   KEY `INDEX` (`fighter_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE  `bets`.`fightodds` (
   `fight_id` int(10) unsigned NOT NULL default '0',
@@ -49,7 +49,7 @@ CREATE TABLE  `bets`.`fightodds` (
   `bookie_id` int(10) unsigned NOT NULL default '0',
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  USING BTREE (`fight_id`,`bookie_id`,`date`,`fighter1_odds`,`fighter2_odds`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`fights` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -58,32 +58,32 @@ CREATE TABLE  `bets`.`fights` (
   `event_id` int(10) unsigned NOT NULL default '0',
   `is_mainevent` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`linkouts_ext` (
   `bookie_id` int(3) unsigned NOT NULL default '0',
   `click_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `event_id` int(8) unsigned NOT NULL default '0',
   `visitor_ip` varchar(15) NOT NULL default ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`fight_twits` (
   `fight_id` int(10) unsigned NOT NULL,
   `twitdate` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`fight_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`sports` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `bets`.`bookies_changenums` (
   `bookie_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   `changenum` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`bookie_id`)
-) ENGINE = InnoDB;
+) ENGINE = MyISAM;
 
 CREATE TABLE  `bets`.`bookies_proptemplates` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -93,7 +93,7 @@ CREATE TABLE  `bets`.`bookies_proptemplates` (
   `template_neg` varchar(300) NOT NULL,
   `fields_type` int(3) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`bookies_parsers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -104,7 +104,7 @@ CREATE TABLE  `bets`.`bookies_parsers` (
   `mockfile` varchar(45) NOT NULL,
   `cn_urlsuffix` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`prop_types` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -112,7 +112,7 @@ CREATE TABLE  `bets`.`prop_types` (
   `negprop_desc` varchar(300) NOT NULL,
   `is_eventprop` TINYINT(1) DEFAULT 0 NOT NULL, 
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`lines_props` (
   `matchup_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -123,12 +123,12 @@ CREATE TABLE  `bets`.`lines_props` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `team_num` int(2) unsigned NOT NULL,
   PRIMARY KEY (`matchup_id`,`bookie_id`,`date`,`proptype_id`,`team_num`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`alerts_exemptions` (
   `email` varchar(45) NOT NULL,
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE  `bets`.`lines_correlations` (
   `correlation` varchar(200) NOT NULL,
@@ -143,7 +143,7 @@ CREATE TABLE  `bets`.`matchups_unmatched` (
   `log_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` int(2) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`matchup`,`bookie_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `bets`.`matchups_metadata` (
   `matchup_id` INT NOT NULL,
@@ -151,21 +151,21 @@ CREATE TABLE `bets`.`matchups_metadata` (
   `mvalue` VARCHAR(45) NOT NULL,
   `source_bookie_id` INT NOT NULL,
   PRIMARY KEY (`matchup_id`, `mattribute`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `bets`.`events_redditposts` (
   `event_id` INT NOT NULL,
   `reddit_id` VARCHAR(45) NOT NULL,
   `last_change` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `schedule_manualactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(2000) DEFAULT NULL,
   `type` int(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `lines_eventprops` (
   `event_id` int(10) unsigned NOT NULL DEFAULT '0',
