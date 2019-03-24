36c0c1b98a5b387610cdf9e31fc65d1d3a222d0b
Nordvaller <christian.nordvaller@accenture.com>
2019-03-24 23:55:53 +0100

    Updated BetDSI parser to parse new XML

--- app/parsers/xmlparsers/parser.BetDSI.php
+++ app/parsers/xmlparsers/parser.BetDSI.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('lib/bfocore/parser/utils/class.ParseTools.php');
+
 /**
  * XML Parser
  *
@@ -35,66 +37,43 @@ class XMLParserBetDSI
         $aSports = array();
         $oParsedSport = new ParsedSport('MMA');
         
-        foreach ($oXML->PPH as $cDSI)
+        foreach ($oXML->Sport as $sport_node)
         {
-            //Matchups (also contains some props):
-            if ($cDSI->IDLeague == 'Fighting')
+            if ($sport_node['Name'] == 'MMA')
             {
-                    if (ParseTools::checkCorrectOdds((string) $cDSI->HomeMoneyLine) && ParseTools::checkCorrectOdds((string) $cDSI->AwayMoneyLine))
+                foreach ($sport_node->Event as $event_node)
+                {
+                    foreach ($event_node->Match as $match_node)
                     {
-                        //Check if bet is a prop or not
-                        if (ParseTools::isProp((string) $cDSI->HomeTeamName) && ParseTools::isProp((string) $cDSI->AwayTeamName))
+                        $competitors = explode(' - ', $match_node['Name']);
+                        $odds = [];
+                        foreach ($match_node->Bet as $bet_node)
                         {
-                            //Prop, add as such
-                            $oParsedSport->addFetchedProp(new ParsedProp(
-                                            (string) $cDSI->HomeTeamName,
-                                            (string) $cDSI->AwayTeamName,
-                                            (string) $cDSI->HomeMoneyLine,
-                                            (string) $cDSI->AwayMoneyLine
-                            ));
-                        }
-                        else
-                        {
-                            //Not a prop, add as matchup
-
-                            //TODO: Temporary fix for Gaethje v Vick
-                            if ($cDSI->HomeTeamName == 'J VICK' && $cDSI->AwayTeamName == 'J GAETHJE')
+                            if ($bet_node['Name'] == 'Bout Odds')
                             {
-                                $cDSI->HomeTeamName = 'JAMES VICK';
-                                $cDSI->AwayTeamName = 'JUSTIN GAETHJE';
+                                foreach($bet_node->Odd as $odd_node)
+                                {
+                                    $odds[((int) $odd_node['Name']) - 1] = OddsTools::convertDecimalToMoneyline($odd_node['Value']);
+                                }
+                                if (ParseTools::checkCorrectOdds((string) $odds[0]) && ParseTools::checkCorrectOdds((string) $odds[1]))
+                                {
+                                    $oParsedSport->addParsedMatchup(new ParsedMatchup(
+                                        (string) $competitors[0],
+                                        (string) $competitors[1],
+                                        (string) $odds[0],
+                                        (string) $odds[1]
+                                     ));
+                                }
                             }
-
-
-                            $oParsedSport->addParsedMatchup(new ParsedMatchup(
-                                            (string) $cDSI->HomeTeamName,
-                                            (string) $cDSI->AwayTeamName,
-                                            (string) $cDSI->HomeMoneyLine,
-                                            (string) $cDSI->AwayMoneyLine
-                            ));
-
-
-                            //Check if a total is available, if so, add it as a prop
-                            if (isset($cDSI->AwayTotal) && 
-                                isset($cDSI->HomeTotal) && 
-                                isset($cDSI->AwayTotalJuice) && 
-                                isset($cDSI->HomeTotalJuice) && 
-                                trim((string) $cDSI->AwayTotalJuice) != '' && 
-                                trim((string) $cDSI->HomeTotalJuice) != '')
+                            else
                             {
-                                //Total exists, add it
-                                $oParsedProp = new ParsedProp(
-                                              (string) $cDSI->HomeTeamName . ' vs ' . (string) $cDSI->AwayTeamName . ' - OVER ' . $cDSI->AwayTotal,
-                                              (string) $cDSI->HomeTeamName . ' vs ' . (string) $cDSI->AwayTeamName . ' - UNDER ' . $cDSI->AwayTotal,
-                                              (string) $cDSI->AwayTotalJuice,
-                                              (string) $cDSI->HomeTotalJuice);
-                          
-                                $oParsedSport->addFetchedProp($oParsedProp);
+                                Logger::getInstance()->log("New type of bet type: " . $bet_node['Name'] , -1);
                             }
-
                         }
                     }
                 }
             }
+        }
 
         //Declare authorative run if we fill the criteria
         if (count($oParsedSport->getParsedMatchups()) >= 5 && $oParsedSport->getPropCount() >= 2)
