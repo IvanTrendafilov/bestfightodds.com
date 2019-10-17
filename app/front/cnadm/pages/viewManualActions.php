1e4a70daefa72bae43355ee9a412f200104bca2a
cn@capapps.se <cn@capapps.se>
2019-10-17 22:33:27 +0200

    Improvements to manual actions and unmatched

--- app/front/cnadm/pages/viewManualActions.php
+++ app/front/cnadm/pages/viewManualActions.php
@@ -23,39 +23,39 @@ if ($aManualActions != null && sizeof($aManualActions) > 0)
 		{
 			case 1:
 			//Create event and matchups
-				echo 'Create new event: ' . $oAction->eventTitle . ' at ' . $oAction->eventDate . ' with matchups: <br />' ;
+				echo 'Create new event: </td><td>' . $oAction->eventTitle . '</td><td> at </td><td>' . $oAction->eventDate . ' with matchups: <br />' ;
 				foreach ($oAction->matchups as $aMatchup)
 				{
 					echo '&nbsp;' . $aMatchup[0] . ' vs ' . $aMatchup[1] . '<br/>';
 				}
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maAddEventWithMatchups(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo '</td><td><input type="submit" value="Accept" onclick="maAddEventWithMatchups(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 2:
 			//Rename event
-				echo 'Rename event ' . EventHandler::getEvent($oAction->eventID)->getName() . ' to ' . $oAction->eventTitle;
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maRenameEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Rename event </td><td>' . EventHandler::getEvent($oAction->eventID)->getName() . '</td><td> to </td><td>' . $oAction->eventTitle;
+				echo '</td><td><input type="submit" value="Accept" onclick="maRenameEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 3:
 			//Change date of event
 				$oEvent = EventHandler::getEvent($oAction->eventID);
-				echo 'Change date of ' . $oEvent->getName() . ' from ' . $oEvent->getDate() . ' to ' . $oAction->eventDate;
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maRedateEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Change date of </td><td>' . $oEvent->getName() . '</td><td> from </td><td>' . $oEvent->getDate() . '</td><td> to </td><td>' . $oAction->eventDate;
+				echo '</td><td><input type="submit" value="Accept" onclick="maRedateEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 4:
 			//Delete event
 				$oEvent = EventHandler::getEvent($oAction->eventID);
-				echo 'Delete event ' . $oEvent->getName() . ' - ' . $oEvent->getDate();
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maDeleteEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Delete event </td><td>' . $oEvent->getName() . ' - ' . $oEvent->getDate();
+				echo '</td><td><input type="submit" value="Accept" onclick="maDeleteEvent(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 5:
 			//Create matchup
 				$oEvent = EventHandler::getEvent($oAction->eventID);
-				echo 'Create matchup ' . $oAction->matchups[0]->team1 . ' vs. ' . $oAction->matchups[0]->team2 . ' at ' . EventHandler::getEvent($oAction->eventID)->getName();
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maAddMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Create matchup </td><td>' . $oAction->matchups[0]->team1 . ' vs. ' . $oAction->matchups[0]->team2 . '</td><td> at </td><td>' . EventHandler::getEvent($oAction->eventID)->getName();
+				echo '</td><td><input type="submit" value="Accept" onclick="maAddMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 6:
@@ -63,30 +63,30 @@ if ($aManualActions != null && sizeof($aManualActions) > 0)
 				$oMatchup = EventHandler::getFightByID($oAction->matchupID);
 				$oOldEvent = EventHandler::getEvent($oMatchup->getEventID());
 				$oNewEvent = EventHandler::getEvent($oAction->eventID);
-				echo 'Move ' . $oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2) . ' from ' . $oOldEvent->getName() . ' (' . $oOldEvent->getDate() . ') to ' . $oNewEvent->getName() . ' (' . $oNewEvent->getDate() . ')';
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maMoveMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Move </td><td>' . $oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2) . '</td><td> from </td><td>' . $oOldEvent->getName() . ' (' . $oOldEvent->getDate() . ')</td><td> to </td><td>' . $oNewEvent->getName() . ' (' . $oNewEvent->getDate() . ')';
+				echo '</td><td><input type="submit" value="Accept" onclick="maMoveMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 7:
 			//Delete matchup
 				$oMatchup = EventHandler::getFightByID($oAction->matchupID);
 				$oEvent = EventHandler::getEvent($oMatchup->getEventID());
-				echo 'Delete <a href="http://www.google.com/search?q=' . urlencode($oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2)) . '">' . $oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2) . '</a> from ' . $oEvent->getName() . ' (' . $oEvent->getDate() .')';
-				echo '<input type="submit" value="Accept" style="float: right" onclick="maDeleteMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
+				echo 'Delete </td><td><a href="http://www.google.com/search?q=' . urlencode($oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2)) . '">' . $oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2) . '</a></td><td> from </td><td>' . $oEvent->getName() . ' (' . $oEvent->getDate() .')';
+				echo '</td><td><input type="submit" value="Accept" onclick="maDeleteMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($aManualAction['description']). '\')" />
 				';
 			break;
 			case 8:
 			//Move matchup to a non-existant event
 			
 				$oNewEvent = EventHandler::getEventByName($oAction->eventTitle);
-				echo 'Move the following matchups to ' . ($oNewEvent != null ? $oNewEvent->getName() : 'TBD') . '<br />
+				echo 'Move the following matchups</td><td> to </td><td>' . ($oNewEvent != null ? $oNewEvent->getName() : 'TBD') . '<br />
 ';
 				foreach ($oAction->matchupIDs as $sMatchup)
 				{
 					$oMatchup = EventHandler::getFightByID($sMatchup);
 					echo '&nbsp;' . $oMatchup->getTeamAsString(1) . ' vs. ' . $oMatchup->getTeamAsString(2). '';
 					$newMA = json_encode(array('matchupID' => $sMatchup, 'eventID' => ($oNewEvent != null ? $oNewEvent->getID() : '-9999')), JSON_HEX_APOS | JSON_HEX_QUOT);
-					echo '<input type="submit" value="Accept" ' .  ($oNewEvent == null ? ' disabled=true ' : '') . ' style="float: right" onclick="maMoveMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($newMA). '\')" /><br/>
+					echo '</td><td><input type="submit" value="Accept" ' .  ($oNewEvent == null ? ' disabled=true ' : '') . ' onclick="maMoveMatchup(' . $aManualAction['id'] . ', \'' . htmlspecialchars($newMA). '\')" /><br/>
 ';					
 				}
 
