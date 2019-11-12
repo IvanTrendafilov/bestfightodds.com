34fd92f4e4bbf49367f4a40b62d881e9bf45e223
cn@capapps.se <cn@capapps.se>
2019-11-12 23:21:10 +0100

    Testmail admin feature

--- pbo/config/inc.config-template.php
+++ pbo/config/inc.config-template.php
@@ -25,6 +25,7 @@ define('ALERTER_ENABLED', false);
 define('ALERTER_ADMIN_ALERT','cnordvaller@gmail.com');  //E-mail to alert in case of internal alerts (arbitrage etc.)
 define('ALERTER_DEV_MODE', true); //Dev mode on or off, if true no e-mails will be sent
 define('ALERTER_MAIL_FROM', 'Best Fight Odds <no-reply@bestfightodds.com>');
+define('ALERTER_MAIL_SENDER_MAIL', 'no-reply@bestfightodds.com');
 define('ALERTER_SITE_LINK', 'https://www.bestfightodds.com');
 define('ALERTER_SITE_NAME', 'Best Fight Odds');
 
