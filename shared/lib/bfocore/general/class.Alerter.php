3bbad4a934081aa343b23b8fbeca44f3fa3bf416
cn@capapps.se <cn@capapps.se>
2019-12-07 22:46:13 +0100

    Minor Alerter fix

--- shared/lib/bfocore/general/class.Alerter.php
+++ shared/lib/bfocore/general/class.Alerter.php
@@ -169,7 +169,7 @@ Good luck!\n
         {
             //Send e-mail alert
             $mailer = new SESMailer(MAIL_SMTP_HOST, MAIL_SMTP_PORT, MAIL_SMTP_USERNAME, MAIL_SMTP_PASSWORD);
-            $bSuccess = $mailer->sendMail(ALERTER_MAIL_SENDER_MAIL, ALERTER_MAIL_FROM, 'cnordvaller@gmail.com', $sSubject, $sText);       
+            $bSuccess = $mailer->sendMail(ALERTER_MAIL_SENDER_MAIL, ALERTER_MAIL_FROM, 'cnordvaller@gmail.com', $sSubject, $sText, $sText);       
             //$bSuccess = mail($sTo, $sSubject, $sText, $sHeaders);
 
         }
