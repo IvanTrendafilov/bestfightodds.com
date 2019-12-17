950bd3dbe5c6db7ca326e3964f1c3494bc2eb116
cn@capapps.se <cn@capapps.se>
2019-12-17 23:51:07 +0100

    Chart fixes for timezone

--- pbo/app/front/js/bfo_charts.js
+++ pbo/app/front/js/bfo_charts.js
@@ -126,8 +126,7 @@ function createChart(indata) {
                 }
                 
                 //Format and adjust date according to user timezone
-                var now = new Date();
-                var da = new Date(this.x - (now.getTimezoneOffset() * 60000));
+                var da = new Date(this.x);
 
                 var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
                 var monthNames = [
