8fd9618f623c30b919b8b0f2157a0a90aee2a603
cn@capapps.se <cn@capapps.se>
2019-12-25 13:13:25 +0100

    Added install components script

---
+++ shared/env/install-components.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This file will install all components required by the BFO platform. Note that this only needs to be done once per server (not once per site like bestfightodds.com or proboxingodds.com)
+# It is safe to run this multiple times
+#
+# Before running, ensure that you are a user that has sudo rights
+
+# Update Package Index
+sudo apt update
+
+# Install nginx
+sudo apt install -y nginx
+
+# Install python-pip (required for aws cli), 
+sudo apt install -y python-pip
+
+# Install apache utils (for .htpasswd generation)
+sudo apt install -y apache2-utils
+
+# Install PHP 7.2
+sudo apt install -y php7.2
+
+# Install PHP extensions (Mysqli, GD, Mysql, Curl, XML, mbstring)
+sudo apt install -y php7.2-mysqli php-gd php7.2-mysql php-curl php7.2-xml php-mbstring
+
+# Install Composer PHP
+curl -sS https://getcomposer.org/installer -o ~/composer-setup.php
+sudo php ~/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm -f ~/composer-setup.php
+
+# Install AWS CLI via PIP
+sudo pip install awscli
