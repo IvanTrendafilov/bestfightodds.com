70ef5bdfecbaf3be23644cc4e2f84c350d24dfd0
cn@capapps.se <cn@capapps.se>
2019-12-30 16:59:43 +0100

    Revised name formatter to handle III and more

--- shared/lib/bfocore/general/datatypes/class.Fight.php
+++ shared/lib/bfocore/general/datatypes/class.Fight.php
@@ -147,33 +147,34 @@ class Fight
      */
     public function getFighterAsStringFromString($a_sFighter)
     {
-        $sTempFighter = strtolower($a_sFighter);
-        $aNames = explode(' ', $sTempFighter);
-        $sTempFighter = '';
-        foreach ($aNames as $sName)
-        {
-            $sName[0] = strtoupper($sName[0]);
+        $string = $a_sFighter;
 
-            // "McDonald", "O'Conner"..
-            if (strncmp($sName,'Mc',2) == 0 || preg_match('/[OD]\'[a-zA-Z]/', $sName)) 
-            {
-                $sName[2] = strtoupper($sName[2]);
-            }
-
-            // "B.J. Penn"
-            $iPos = strrpos($sName, '.');
-            if ($iPos && isset($sName[$iPos - 2]) && $sName[$iPos - 2] == '.')
-            {
-                $sName[$iPos - 1] = strtoupper($sName[$iPos - 1]);
-            }
-            $iPos = strrpos($sName, '-');
-            if ($iPos)
-            {
-                $sName[$iPos + 1] = strtoupper($sName[$iPos + 1]);
-            }
-            $sTempFighter .= $sName . ' ';
-        }
-        return trim($sTempFighter);
+		$word_splitters = array(' ', '.', '-', "O'", "L'", "D'", 'St.', 'Mc');
+		$lowercase_exceptions = array('the', 'van', 'den', 'von', 'und', 'der', 'de', 'da', 'of', 'and', "l'", "d'");
+		$uppercase_exceptions = array('III', 'IV', 'VI', 'VII', 'VIII', 'IX');
+	
+		$string = strtolower($string);
+		foreach ($word_splitters as $delimiter)
+		{ 
+			$words = explode($delimiter, $string); 
+			$newwords = array(); 
+			foreach ($words as $word)
+			{ 
+				if (in_array(strtoupper($word), $uppercase_exceptions))
+					$word = strtoupper($word);
+				else
+				if (!in_array($word, $lowercase_exceptions))
+					$word = ucfirst($word); 
+	
+				$newwords[] = $word;
+			}
+	
+			if (in_array(strtolower($delimiter), $lowercase_exceptions))
+				$delimiter = strtolower($delimiter);
+	
+			$string = join($delimiter, $newwords); 
+		} 
+		return trim($string); 
     }
 
     /**
--- shared/lib/bfocore/general/datatypes/class.Fighter.php
+++ shared/lib/bfocore/general/datatypes/class.Fighter.php
@@ -25,39 +25,37 @@ class Fighter
 	{
 		return $this->iID;
 	}
-	
-	/**
-	 * Use this for a nicer representation of the name. Only when displaying and when using though!
-	 */
-	public function getNameAsString()
-	{
-        $sTempFighter = strtolower($this->sName);
-		$aNames = explode(' ', $sTempFighter);
-		$sTempFighter = '';
-		foreach ($aNames as $sName)
-		{
-			$sName[0] = strtoupper($sName[0]);
-	
-            // "McDonald", "O'Conner"..
-            if (strncmp($sName,'Mc',2) == 0 || preg_match('/[OD]\'[a-zA-Z]/', $sName)) 
-            {
-                $sName[2] = strtoupper($sName[2]);
-            }
 
-            // "B.J. Penn"
-            $iPos = strrpos($sName, '.');
-            if ($iPos && $sName[$iPos - 2] == '.')
-			{
-				$sName[$iPos - 1] = strtoupper($sName[$iPos - 1]);
+	function getNameAsString() 
+	{
+		$string = $this->sName;
+
+		$word_splitters = array(' ', '.', '-', "O'", "L'", "D'", 'St.', 'Mc');
+		$lowercase_exceptions = array('the', 'van', 'den', 'von', 'und', 'der', 'de', 'da', 'of', 'and', "l'", "d'");
+		$uppercase_exceptions = array('III', 'IV', 'VI', 'VII', 'VIII', 'IX');
+	
+		$string = strtolower($string);
+		foreach ($word_splitters as $delimiter)
+		{ 
+			$words = explode($delimiter, $string); 
+			$newwords = array(); 
+			foreach ($words as $word)
+			{ 
+				if (in_array(strtoupper($word), $uppercase_exceptions))
+					$word = strtoupper($word);
+				else
+				if (!in_array($word, $lowercase_exceptions))
+					$word = ucfirst($word); 
+	
+				$newwords[] = $word;
 			}
-			$iPos = strrpos($sName, '-');
-			if ($iPos)
-			{
-				$sName[$iPos + 1] = strtoupper($sName[$iPos + 1]);
-			}
-			$sTempFighter .= $sName . ' ';
-		}
-		return trim($sTempFighter);
+	
+			if (in_array(strtolower($delimiter), $lowercase_exceptions))
+				$delimiter = strtolower($delimiter);
+	
+			$string = join($delimiter, $newwords); 
+		} 
+		return trim($string); 
 	}
 	
 	public function getFighterAsLinkString()
