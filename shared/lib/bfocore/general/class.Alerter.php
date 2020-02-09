dbf1e255a60e31040957d6b900126ecd591b62fe
cn@capapps.se <cn@capapps.se>
2020-02-09 17:40:07 +0100

    Alerter message body fix for HTML

--- shared/lib/bfocore/general/class.Alerter.php
+++ shared/lib/bfocore/general/class.Alerter.php
@@ -140,6 +140,8 @@ Check out " . ALERTER_SITE_LINK . " to view the latest listings.\n
 You are receiving this e-mail because you have signed up to be notified when odds were added for a certain matchup. If you did not sign up for this you don't have to do anything as your e-mail will not be stored for future use.\n
 Good luck!\n
 " . ALERTER_SITE_NAME;
+
+            $sMessageHTML = "Odds for " . $oFight->getFighterAsString(1) . " (" . $sTeamOdds[1] . ") vs " . $oFight->getFighterAsString(2) . " (" . $sTeamOdds[2] . ") has just been posted at " . ALERTER_SITE_NAME . "<br>";
             $sSubject = 'Odds for ' . $oFight->getFighterAsString(1) . ' vs ' . $oFight->getFighterAsString(2) . ' available';
         } else
         {
@@ -148,6 +150,9 @@ Check out " . ALERTER_SITE_LINK . " to view the latest listings.\n
 You are receiving this e-mail because you have signed up to be notified when the odds changed for a certain matchup. If you did not sign up for this you don't have to do anything as your e-mail will not be stored for future use.\n
 Good luck!\n
 " . ALERTER_SITE_NAME;
+
+
+            $sMessageHTML = "The odds for " . $oFight->getFighterAsString($a_oAlert->getFighter()) . " has reached " . $sTeamOdds[$a_oAlert->getFighter()] . " in his/her upcoming fight against " . $oFight->getFighterAsString(($a_oAlert->getFighter() == 1 ? 2 : 1)) . "<br>";
             $sSubject = 'Odds for ' . $oFight->getFighterAsString($a_oAlert->getFighter()) . ' has reached your limit';
         }
         $sTo = $a_oAlert->getEmail();
@@ -159,7 +164,7 @@ Good luck!\n
         $sTextHTML = fread($rFile, filesize(ALERTER_TEMPLATE_DIR . '/alertmail.html'));
         fclose($rFile);
 
-        $sTextHTML = str_replace('{{MESSAGE}}', str_replace('\n','<br>', $sText), $sTextHTML);
+        $sTextHTML = str_replace('{{MESSAGE}}', $sMessageHTML, $sTextHTML);
         $sTextHTML = str_replace('{{SITENAME}}', ALERTER_SITE_NAME, $sTextHTML);
         $sTextHTML = str_replace('{{SUBJECT}}', $sSubject, $sTextHTML);
         $sTextHTML = str_replace('{{SITEURL}}', ALERTER_SITE_LINK, $sTextHTML);
