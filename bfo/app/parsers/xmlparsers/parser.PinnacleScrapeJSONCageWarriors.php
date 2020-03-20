df3fe5160d9e81b3af0daf2a0a314e250f53c689
Nordvaller <cn@capapps.se>
2020-03-20 11:58:00 +0100

    Pinnacle Cage Warriors scraper

---
+++ bfo/app/parsers/xmlparsers/parser.PinnacleScrapeJSONCageWarriors.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once('lib/bfocore/parser/utils/class.ParseTools.php');
+require_once('lib/simple_html_dom/simple_html_dom.php');
+
+/**
+ * XML Parser
+ *
+ * Bookie: Pinnacle Site JSON parser
+ * Sport: MMA
+ *
+ * Comment: Used when API us unavailable. Identical to its twin but this is for Bellator
+ *
+ */
+class XMLParserPinnacleScrapeJSONCageWarriors
+{
+    private $oParsedSport;
+    private $bAuthorativeRun = false;
+
+    public function __construct()
+    {
+        $this->oParsedSport = new ParsedSport('MMA');
+    }
+
+    public function parseXML($a_sXML)
+    {
+        //Actually JSON
+        $json = json_decode($a_sXML, true);
+
+        foreach ($json['Leagues'] as $league);
+        {
+            foreach ($league['Events'] as $event)
+            {
+                if (count($event['Participants']) == 2)
+                {
+                    //Regular matchup
+                    $oParsedMatchup = new ParsedMatchup(
+                        $event['Participants'][0]['Name'],
+                        $event['Participants'][1]['Name'],
+                        round($event['Participants'][0]['MoneyLine']),
+                        round($event['Participants'][1]['MoneyLine'])
+                    );
+
+                    $this->oParsedSport->addParsedMatchup($oParsedMatchup);
+                }
+            }
+        }
+
+        return [$this->oParsedSport];
+    }
+
+    public function checkAuthoritiveRun($a_aMetadata)
+    {
+        return $this->bAuthorativeRun;
+    }
+}
+?>
+
