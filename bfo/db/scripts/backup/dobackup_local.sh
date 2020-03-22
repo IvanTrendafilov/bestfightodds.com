8ece0e75fde0144bc354bc6957c2c61052b7f307
Nordvaller <cn@capapps.se>
2020-03-22 14:42:42 +0100

    Added local backup script

---
+++ bfo/db/scripts/backup/dobackup_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+backupdir="/var/www/vhosts/bestfightodds.com/httpdocs/db/scripts/backup/"
+backupfilename="bfo2_backup_$(date '+%Y%m%d%H%M%S')"
+
+echo 'Starting backup'
+cd $backupdir/
+mysqldump -ubestfightodds -pmUCe5haj --skip-tz-utc bets > $backupdir/$backupfilename.sql
+echo 'Dump complete'
+zip -j -9 -D $backupdir/$backupfilename.zip $backupdir/$backupfilename.sql
+echo 'Compression complete'
+rm $backupfilename.sql
