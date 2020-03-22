64550210bdeff1892b069c508e6b4a494292ae0b
Nordvaller <cn@capapps.se>
2020-03-22 17:34:02 +0100

    Updated backup script for AWS

--- bfo/db/scripts/backup/dobackup_local.sh
+++ bfo/db/scripts/backup/dobackup_local.sh
@@ -5,7 +5,7 @@ backupfilename="bfo2_backup_$(date '+%Y%m%d%H%M%S')"
 
 echo 'Starting backup'
 cd $backupdir/
-mysqldump -ubestfightodds -pmUCe5haj --skip-tz-utc bets > $backupdir/$backupfilename.sql
+mysqldump -h ls-9bf1268dd5beed1d539ee8d4c86139bf072394e4.cc2cczpln4tj.us-east-2.rds.amazonaws.com -ubfo -peVhT03rCIYT7u3c bets > $backupdir/$backupfilename.sql
 echo 'Dump complete'
 zip -j -9 -D $backupdir/$backupfilename.zip $backupdir/$backupfilename.sql
 echo 'Compression complete'
