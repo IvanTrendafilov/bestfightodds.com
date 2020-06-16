64f0908a46623ca2c237189b771d31fc14e9df25
Christian Nordvaller <cn@capapps.se>
2020-06-16 17:22:48 +0200

    BetDSI Temp parser

--- bfo/app/parsers/xmlparsers/parser.BetDSITemp.php
+++ bfo/app/parsers/xmlparsers/parser.BetDSITemp.php
@@ -15,7 +15,7 @@ require_once('lib/simple_html_dom/simple_html_dom.php');
  * Props: Yes (confirmed)
  * Authoritative run: Yes
  *
- * Comment: Temporary version when regular is unavailable
+ * Comment: Temporary version when lines.betdsi.eu is not available. This one uses the JSON feed provided when logging in
  *
  */
 class XMLParserBetDSITemp
@@ -24,46 +24,43 @@ class XMLParserBetDSITemp
 
     public function parseXML($a_sPage)
     {
-        $html = new simple_html_dom();
-        $html->load($a_sPage);
-
+        $aSports = array();
         $oParsedSport = new ParsedSport('MMA');
 
-        if ($html == false)
-        {
-            Logger::getInstance()->log("Warning: HTML broke!!", -1);
-        }
+        //Actually JSON
+        $json = json_decode($a_sPage, true);
 
-        foreach ($html->find("table.tableLines") as $tables) 
+        foreach ($json as $matchup)
         {
-            $rows = $tables->find("tr");
-            $team1name = (string) trim($rows[1]->find('td.team')[0]->plaintext);
-            $team2name = (string) trim($rows[2]->find('td.team')[0]->plaintext);
-            
-            $team1odds = (string) $rows[1]->find('td.total')[0]->plaintext;
-            $team2odds = (string) $rows[2]->find('td.total')[0]->plaintext;
-
-            if (ParseTools::checkCorrectOdds((string) $team1odds) &&
-                ParseTools::checkCorrectOdds((string) $team2odds))
+            if ($matchup['SportType']['Name'] == 'MMA' && $matchup['IsLive'] == false) 
             {
-                $oParsedSport->addParsedMatchup(new ParsedMatchup(
-                                $team1name,
-                                $team2name,
-                                $team1odds,
-                                $team2odds
-                ));
-            }
-               
-        }
-        
-        //Declare authorative run if we fill the criteria
-        if (false)
-        {
-            $this->bAuthorativeRun = true;
-            Logger::getInstance()->log("Declared authoritive run", 0);
-        }
+                $oParsedMatchup = new ParsedMatchup(
+                    $matchup['PreviewOddsMoneyLine'][0]['Title'],
+                    $matchup['PreviewOddsMoneyLine'][1]['Title'],
+                    OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsMoneyLine'][0]['Value']),
+                    OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsMoneyLine'][1]['Value'])
+                );
+                $oParsedMatchup->setCorrelationID((string) $matchup['ID']);
+                $oParsedSport->addParsedMatchup($oParsedMatchup);
 
-        return [$oParsedSport];
+                //Add total if available
+                if (isset($matchup['PreviewOddsTotal']) && count($matchup['PreviewOddsTotal']) == 2)
+                {
+                    $oParsedProp = new ParsedProp(
+                        $matchup['Name'] . ' : ' . $matchup['PreviewOddsTotal'][0]['Title'] . ' rounds',
+                        $matchup['Name'] . ' : ' . $matchup['PreviewOddsTotal'][1]['Title'] . ' rounds',
+                        OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsTotal'][0]['Value']),
+                        OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsTotal'][1]['Value'])
+                    );
+
+                    //Add correlation ID
+                    $oParsedProp->setCorrelationID((string) $matchup['ID']);
+                    $oParsedSport->addFetchedProp($oParsedProp);
+                }
+            }
+        }
+ 
+        return $oParsedSport;
     }
 
     public function checkAuthoritiveRun($a_aMetadata)
