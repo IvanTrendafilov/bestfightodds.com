e75b358766ffb96952e24f968ae957035c07e0b3
Christian Nordvaller <cn@capapps.se>
2020-07-15 10:36:47 +0200

    5Dimes check to remove Jack Shore dupe

--- bfo/app/parsers/xmlparsers/parser.5Dimes.php
+++ bfo/app/parsers/xmlparsers/parser.5Dimes.php
@@ -46,7 +46,8 @@ class XMLParser5Dimes
                     && ((int) $cEvent->IsCancelled) != 1
                     && ((int) $cEvent->isGraded) != 1)
                     && !((trim((string) $cEvent->HomeMoneyLine) == '-99999') && (trim((string) $cEvent->VisitorMoneyLine) == '-99999'))
-                    && !strpos(strtolower((string)$cEvent->Header), 'boxing propositions')
+                    && !strpos(strtolower((string)$cEvent->Header), 'boxing propositions'
+                    && !($cEvent->GameNumber == '637517256' && (float) $cEvent->HomeMoneyLineEU > 1.2)) //Temporary jack shore dupe remove
             )
             {
 
