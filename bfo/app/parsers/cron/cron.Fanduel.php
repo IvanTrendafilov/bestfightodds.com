f272b0abc759ce68519fc650d13e987db3d3de8e
cnordvaller <cn@capapps.se>
2021-02-14 19:41:43 +0100

    Reordered options

--- bfo/app/parsers/cron/cron.Fanduel.php
+++ bfo/app/parsers/cron/cron.Fanduel.php
@@ -62,11 +62,11 @@ class ParserJob
         try 
         {
             $client = Client::createChromeClient(null, [
+                '--no-sandbox',
                 '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
                 '--window-size=1200,1100',
                 '--headless',
                 '--disable-gpu',
-                '--no-sandbox',
             ], ['port' => intval('95' . BOOKIE_ID)]);
             $client->request('GET', 'https://sportsbook.fanduel.com/sports/navigation/7287.1/9886.3');
 
