6ef9063cb7d40bfc9ef01133b22b0b80fedc4a50
cnordvaller <cn@capapps.se>
2021-02-18 12:43:47 +0100

    Fanduel test

--- bfo/app/parsers/cron/cron.Fanduel.php
+++ bfo/app/parsers/cron/cron.Fanduel.php
@@ -65,11 +65,7 @@ class ParserJob
         {
 
 
-            $client = Client::createFirefoxClient(null, ['--no-sandbox',
-            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-            '--window-size=1200,1100',
-            '--headless',
-            '--disable-gpu'],
+            $client = Client::createFirefoxClient(null, null,
              ['port' => intval('95' . BOOKIE_ID), 'host' => 'localhost', 'capabilities' => ['platform' => 'linux']]);
             /*$client = Client::createChromeClient(null, [
                 '--no-sandbox',
