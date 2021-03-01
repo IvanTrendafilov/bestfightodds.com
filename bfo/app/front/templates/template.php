6ffca1597a2558c4134781721535d46b7b79da51
cnordvaller <cn@capapps.se>
2021-03-01 20:38:33 +0100

    Ipad template fix

--- bfo/app/front/templates/template.php
+++ bfo/app/front/templates/template.php
@@ -32,6 +32,7 @@
                 @media only screen and (max-width: 768px) {
                     .odds-table {
                         font-size: 10px;
+                        -webkit-text-size-adjust: none;
                     }
                 }
             </style>
