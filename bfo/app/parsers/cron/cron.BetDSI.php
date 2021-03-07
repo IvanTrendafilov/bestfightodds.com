4e1c66a65c79b49a8b6ff01c3f1de65114da1083
cnordvaller <cn@capapps.se>
2021-03-07 14:37:41 +0100

    Update BetDSI cron

--- bfo/app/parsers/cron/cron.BetDSI.php
+++ bfo/app/parsers/cron/cron.BetDSI.php
@@ -45,8 +45,8 @@ class ParserJob
         $content = null;
         if ($mode == 'mock')
         {
-            $this->logger->info("Note: Using matchup mock file at " . PARSE_MOCKFEEDS_DIR . "betdsi.xml");
-            $content = ParseTools::retrievePageFromFile(PARSE_MOCKFEEDS_DIR . 'betdsi.xml');
+            $this->logger->info("Note: Using matchup mock file at " . PARSE_MOCKFEEDS_DIR . "betdsi.json");
+            $content = ParseTools::retrievePageFromFile(PARSE_MOCKFEEDS_DIR . 'betdsi.json');
         }
         else
         {
@@ -72,131 +72,56 @@ class ParserJob
 
     public function parseContent($content)
     {
-        $oXML = simplexml_load_string($content);
-        if ($oXML == false)
-        {
-            $this->logger->warning("Warning: XML broke!!");
-        }
-
-        //Custom sort function to arrange bet nodes
-        function odd_node_sort($a, $b)
-        {
-            if ((int) $a['Name'] == (int) $b['Name']) return 0;
-            return ((int)$a['Name'] < (int)$b['Name'])?-1:1;
-        }
-
         $oParsedSport = new ParsedSport('MMA');
+        $json = json_decode($content, true);
 
-        $feedtime = new DateTime(trim($oXML->Date));
-        $nowtime = new DateTime('now');
-        $interval = date_diff($feedtime, $nowtime);
-        $this->logger->info("Feed date: " . trim($oXML->Date) . " , " . $interval->days . " old");
-
-        if (trim($oXML->Date) == 'Fri May  1 13:50:02 CST 2020' || trim($oXML->Date) == 'Fri May  8 04:25:01 CST 2020' || trim($oXML->Date) == 'Sat May 30 15:40:01 CST 2020')
+        foreach ($json as $matchup)
         {
-            $this->logger->warning("Old feed detected. (" . trim($oXML->Date) . ") Bailing");
-            return $oParsedSport;
-        }
-
-        //Store as latest feed available for ProBoxingOdds.com
-        $file = fopen(GENERAL_BASEDIR . '/app/front/externalfeeds/betdsi-latest.xml', 'w');
-        fwrite($file, $content);
-        fclose($file);
-        
-        foreach ($oXML->Sport as $sport_node)
-        {
-            if ($sport_node['Name'] == 'MMA')
+            if (($matchup['SportType']['Name'] == 'MMA' || $matchup['Category']['Name'] == 'UFC' || $matchup['Category']['Name'] == 'LFA' || $matchup['Category']['Name'] == 'Bellator' || $matchup['Category']['Name'] == 'Invicta') && $matchup['IsLive'] == false) 
             {
-                foreach ($sport_node->Event as $event_node)
+                //Fixes flipped names like Gastelum K. into K Gastelum
+                $matchup['HomeTeamName'] = preg_replace('/([a-zA-Z\-\s]+)\s([a-zA-Z])\./', '$2 $1', $matchup['HomeTeamName']);
+                $matchup['AwayTeamName'] = preg_replace('/([a-zA-Z\-\s]+)\s([a-zA-Z])\./', '$2 $1', $matchup['AwayTeamName']);
+
+                $oParsedMatchup = new ParsedMatchup(
+                    $matchup['HomeTeamName'],
+                    $matchup['AwayTeamName'],
+                    OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsMoneyLine'][0]['Value']),
+                    OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsMoneyLine'][1]['Value'])
+                );
+                $oParsedMatchup->setCorrelationID((string) $matchup['ID']);
+                $oParsedSport->addParsedMatchup($oParsedMatchup);
+
+                //Add total if available
+                if (isset($matchup['PreviewOddsTotal']) && count($matchup['PreviewOddsTotal']) >= 2)
                 {
-                    foreach ($event_node->Match as $match_node)
+                    //Loop through pairs of 1.5, 2.5, ..
+                    for ($i = 0; $i < count($matchup['PreviewOddsTotal']); $i += 2)
                     {
-                        if ($match_node['MatchType'] != 'Live')
+                        if ($matchup['PreviewOddsTotal'][$i]['SpecialBetValue'] == $matchup['PreviewOddsTotal'][$i + 1]['SpecialBetValue'])
                         {
-
-                            $competitors = explode(' - ', $match_node['Name']);
-                            $odds = [];
-                            foreach ($match_node->Bet as $bet_node)
-                            {
-                                if ($bet_node['Name'] == 'Bout Odds' || $bet_node['Name'] == 'Match Winner')
-                                {
-                                    //Sort Odd nodes
-                                    $bet_node->Odd = usort($bet_node->Odd,"odd_node_sort");
-
-                                    //Regular matchup odds
-                                    foreach($bet_node->Odd as $odd_node)
-                                    {
-                                        $odds[((int) $odd_node['Name']) - 1] = OddsTools::convertDecimalToMoneyline($odd_node['Value']);
-                                    }
-                                    if (ParseTools::checkCorrectOdds((string) $odds[0]) && ParseTools::checkCorrectOdds((string) $odds[1]))
-                                    {
-                                        $oParsedMatchup = new ParsedMatchup(
-                                            (string) $competitors[0],
-                                            (string) $competitors[1],
-                                            (string) $odds[0],
-                                            (string) $odds[1]
-                                        );
-                                        $oParsedMatchup->setCorrelationID((string) $match_node['Name']);
-
-                                        //Add time of matchup as metadata
-                                        if (isset($match_node['StartDate']))
-                                        {
-                                            $oGameDate = new DateTime($match_node['StartDate']);
-                                            $oParsedMatchup->setMetaData('gametime', $oGameDate->getTimestamp());
-                                        }
-
-                                        //Add header of matchup as metadata
-                                        if (isset($event_node['Name']))
-                                        {
-                                            $oParsedMatchup->setMetaData('event_name', (string) $event_node['Name']);
-                                        }
-
-                                        $oParsedSport->addParsedMatchup($oParsedMatchup);    
-                                    }
-                                }
-                                else
-                                {
-                                    //Prop bet
-                                    if (count($bet_node->Odd) == 2)
-                                    {
-                                        //Probably two way bet (e.g. Yes/No, Over/Under)
-                                        $oParsedProp = new ParsedProp(
-                                            (string) $competitors[0] . ' vs ' . $competitors[1] . ' :: ' . $bet_node->Odd[0]['Name'] . (isset($bet_node->Odd[0]['SpecialBetValue']) ? ' ' . $bet_node->Odd[0]['SpecialBetValue'] : ''),
-                                            (string) $competitors[0] . ' vs ' . $competitors[1] . ' :: ' . $bet_node->Odd[1]['Name'] . (isset($bet_node->Odd[1]['SpecialBetValue']) ? ' ' . $bet_node->Odd[1]['SpecialBetValue'] : ''),
-                                            OddsTools::convertDecimalToMoneyline($bet_node->Odd[0]['Value']),
-                                            OddsTools::convertDecimalToMoneyline($bet_node->Odd[1]['Value'])
-                                        );
-                                        $oParsedProp->setCorrelationID((string) $match_node['Name']);
-                                        $oParsedSport->addFetchedProp($oParsedProp);
-                                    }
-                                    else
-                                    {
-                                        foreach ($bet_node->Odd as $odd_node)
-                                        {
-                                            $oParsedProp = new ParsedProp(
-                                                (string) $competitors[0] . ' vs ' . $competitors[1] . ' :: ' . $odd_node['Name'],
-                                                '',
-                                                OddsTools::convertDecimalToMoneyline($odd_node['Value']),
-                                                '-99999');
-                                            $oParsedProp->setCorrelationID((string) $match_node['Name']);
-                                            $oParsedSport->addFetchedProp($oParsedProp);
-                                        }
-                                    }
-                                }
-                            }
+                            $oParsedProp = new ParsedProp(
+                                $matchup['HomeTeamName'] . ' - ' . $matchup['AwayTeamName'] . ' : ' . $matchup['PreviewOddsTotal'][$i]['Title'] . ' rounds',
+                                $matchup['HomeTeamName'] . ' - ' . $matchup['AwayTeamName'] . ' : ' . $matchup['PreviewOddsTotal'][$i + 1]['Title'] . ' rounds',
+                                OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsTotal'][$i]['Value']),
+                                OddsTools::convertDecimalToMoneyline($matchup['PreviewOddsTotal'][$i + 1]['Value'])
+                            );
+                            //Add correlation ID
+                            $oParsedProp->setCorrelationID((string) $matchup['ID']);
+                            $oParsedSport->addFetchedProp($oParsedProp);
                         }
                     }
                 }
             }
         }
 
-        //Declare full run if we fill the criteria
-        if (count($oParsedSport->getParsedMatchups()) >= 5 && $oParsedSport->getPropCount() >= 2)
+        //Declare authorative run if we fill the criteria
+        if (count($oParsedSport->getParsedMatchups()) > 10)
         {
             $this->full_run = true;
-            $this->logger->info("Declared full run");
+            $this->logger->info("Declared authoritive run");
         }
-
+ 
         return $oParsedSport;
     }
 }
