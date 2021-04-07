5ba22f3054a6fb2465ed472234586d1f5a12cd9a
cnordvaller <cn@capapps.se>
2021-04-07 10:06:03 +0200

    Front logic fix

--- bfo/app/front/ajax/ajax.Interface.php
+++ bfo/app/front/ajax/ajax.Interface.php
@@ -5,8 +5,6 @@
 
 checkRequiredParam('f');
 
-require_once('app/front/pages/inc.FrontLogic.php');
-
 switch($_GET['f'])
 {
     case 'rp':
@@ -281,6 +279,87 @@ function checkRequiredParam($sParamName, $bPrintOut = true)
     }
 
     return true;
+}
+
+function getTimeDifference($a_sStart, $a_sEnd)
+{
+    if ($a_sStart == '')
+    {
+        return 'n/a';
+    }
+
+    if ($a_sStart !== -1 && $a_sEnd !== -1)
+    {
+        if ($a_sEnd >= $a_sStart)
+        {
+            $sRetString = '';
+
+            $diff = $a_sEnd - $a_sStart;
+            if ($days = intval(floor($diff / 86400)))
+            {
+                $diff = $diff % 86400;
+            }
+            if ($hours = intval(floor($diff / 3600)))
+            {
+                $diff = $diff % 3600;
+            }
+            if ($minutes = intval(floor($diff / 60)))
+            {
+                $diff = $diff % 60;
+            }
+            if ($days == 0 && $hours == 0 && $minutes == 0)
+            {
+                $minutes = 1;
+            }
+
+            if ($days > 0)
+            {
+                if ($days == 1)
+                {
+                    $sRetString .= '1 day';
+                    if ($hours > 0)
+                    {
+                        $sRetString .= ' ' . $hours . ' hr';
+                    }
+                    else
+                    {
+                        if ($minutes > 0)
+                        {
+                            $sRetString .= ' ' . $minutes . ' min';
+                        }
+                    }
+                }
+                else
+                {
+                    $sRetString .= $days . ' days';
+                }
+            }
+            else
+            {
+                if ($hours > 0)
+                {
+                    $sRetString .= $hours . ' hr';
+                    if ($minutes > 0)
+                    {
+                        $sRetString .= ' ' . $minutes . ' min';
+                    }
+                }
+                else
+                {
+                    if ($minutes == 1)
+                    {
+                        $sRetString .= '&lt; ';
+                    }
+                    $sRetString .= $minutes . ' min';
+                }
+            }
+
+            $sRetString .= ' ago';
+
+            return $sRetString;
+        }
+    }
+    return '';
 }
 
 
