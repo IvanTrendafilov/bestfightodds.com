e1cd0a4c3aeceb28acada2b6f8e76a73a70ea41d
cnordvaller <cn@capapps.se>
2021-04-09 11:04:32 +0200

    Added runstatus to layout

--- shared/app/admin/templates/base/layout.php
+++ shared/app/admin/templates/base/layout.php
@@ -16,7 +16,7 @@
             <?php $this->insert('base/sidebar') ?>
             
             <div class="flex-1 flex flex-col overflow-hidden">
-                <?php $this->insert('base/header') ?>
+                <?php $this->insert('base/header', ['runstatus' => $runstatus]) ?>
 
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
                     <div class="container mx-auto px-6 py-8">
