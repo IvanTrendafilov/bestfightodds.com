864ba28eff96298cce32854d9673e3ec8af378dc
cnordvaller <cn@capapps.se>
2021-04-10 12:14:56 +0200

    Run status update

--- shared/app/admin/templates/parser_status.php
+++ shared/app/admin/templates/parser_status.php
@@ -1,30 +1,16 @@
-<h3 class="text-gray-700 text-3xl font-medium">Parser status</h3>
-<div class="mt-4">
-    <div class="flex flex-wrap">
-        <?php foreach ($runstatus as $runstatus_entry) : ?>
-            <a href="/cnadm/parserlogs/<?= strtolower($runstatus_entry['name']) ?>">
-                <div class="px-2 mb-2 w-52">
-                    <div class="flex items-center px-5 py-3 shadow-sm rounded-md bg-white">
-                        <div class="p-3 rounded-full <?= ($runstatus_entry['average_matched'] <= 0 ? 'bg-red-500' : 'bg-green-500') ?> bg-opacity-75">
+<h3>Parser status</h3>
+<?php foreach ($runstatus as $runstatus_entry) : ?>
+    <a href="/cnadm/parserlogs/<?= strtolower($runstatus_entry['name']) ?>">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0"><?= $runstatus_entry['name'] ?></h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text"><?= $runstatus_entry['average_matched'] ?></p>
+                <span class="badge <?= ($runstatus_entry['average_matched'] <= 0 ? 'bg-danger' : 'bg-success') ?>">Status</span>
+            </div>
+        </div>
 
-                            <svg class="h-4 w-4 text-white" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z" fill="currentColor" />
-                                <path d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z" fill="currentColor" />
-                                <path d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z" fill="currentColor" />
-                                <path d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z" fill="currentColor" />
-                                <path d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z" fill="currentColor" />
-                                <path d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z" fill="currentColor" />
-                            </svg>
+    </a>
 
-                        </div>
-
-                        <div class="mx-5">
-                            <h4 class="font-semibold text-gray-700"><?= $runstatus_entry['name'] ?></h4>
-                            <div class="<?= ($runstatus_entry['average_matched'] <= 0 ? 'text-red-500' : 'text-green-500') ?>"><?= $runstatus_entry['average_matched'] ?></div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        <?php endforeach ?>
-    </div>
-</div>
+<?php endforeach ?>
