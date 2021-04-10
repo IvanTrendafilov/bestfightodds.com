bf7d3e989ae58976c64dddb0147ffa4cae5e2a5d
cnordvaller <cn@capapps.se>
2021-04-10 12:18:23 +0200

    Run status update

--- shared/app/admin/templates/parser_status.php
+++ shared/app/admin/templates/parser_status.php
@@ -1,16 +1,16 @@
 <h3>Parser status</h3>
-<?php foreach ($runstatus as $runstatus_entry) : ?>
-    <a href="/cnadm/parserlogs/<?= strtolower($runstatus_entry['name']) ?>">
-        <div class="card">
-            <div class="card-header">
-                <h5 class="card-title mb-0"><?= $runstatus_entry['name'] ?></h5>
-            </div>
-            <div class="card-body">
-                <p class="card-text"><?= $runstatus_entry['average_matched'] ?></p>
-                <span class="badge <?= ($runstatus_entry['average_matched'] <= 0 ? 'bg-danger' : 'bg-success') ?>">Status</span>
-            </div>
+<div class="row">
+    <?php foreach ($runstatus as $runstatus_entry) : ?>
+        <div class="col-12 col-md-6 col-lg-4">
+            <a href="/cnadm/parserlogs/<?= strtolower($runstatus_entry['name']) ?>">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text"><?= $runstatus_entry['name'] ?> - <?= $runstatus_entry['average_matched'] ?></p>
+                        <span class="badge <?= ($runstatus_entry['average_matched'] <= 0 ? 'bg-danger' : 'bg-success') ?>">Status</span>
+                    </div>
+                </div>
+
+            </a>
         </div>
-
-    </a>
-
-<?php endforeach ?>
+    <?php endforeach ?>
+</div>
