8fdc82fa8eeea46aab6039952826ba8067d178d1
cnordvaller <cn@capapps.se>
2021-04-17 20:23:26 +0200

    Added new cnadm to PBO

--- pbo/app/front/cnadm/index.php
+++ pbo/app/front/cnadm/index.php
@@ -1,61 +1,87 @@
 <?php
 
-//Disable caching
-header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
-header('Expires: Mon, 12 Jul 1996 04:11:00 GMT'); //Any date passed.
-header('Pragma: no-cache');
+use DI\Container;
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Middleware\SessionCookie;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use League\Plates\Engine;
 
-?>
+require 'vendor/autoload.php';
+require 'app/admin/controllers/admin_controller.php';
+require 'app/admin/controllers/admin_api_controller.php';
+require 'app/admin/middleware/auth_middleware.php';
+require 'app/admin/middleware/session_middleware.php';
+
+$container = (new \DI\ContainerBuilder())
+  ->useAutowiring(true)
+  ->addDefinitions([
+    \League\Plates\Engine::class => function () {
+      return new League\Plates\Engine(__DIR__ . '/../../../../shared/app/admin/templates/');
+    }
+  ])
+  ->build();
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$app->add(SessionMiddleware::class);
+
+$app->setBasePath('/cnadm');
+
+//Login/logout
+$app->get('/lin', \AdminController::class . ':loginPage');
+$app->post('/lin', \AdminController::class . ':login');
+$app->get('/logout', \AdminController::class . ':logout');
+
+//Page routes
+$app->group('', function (RouteCollectorProxy $group) {
+  $group->get('[/]', \AdminController::class . ':home');
+  $group->get('/manualactions', \AdminController::class . ':viewManualActions');
+  $group->get('/newmatchup', \AdminController::class . ':createMatchup');
+  $group->get('/events[/{show}]', \AdminController::class . ':eventsOverview');
+  $group->get('/fighters/{id}', \AdminController::class . ':viewFighter');
+  $group->get('/addOddsManually', \AdminController::class . ':addOddsManually');
+  $group->get('/clearOddsForMatchupAndBookie', \AdminController::class . ':clearOddsForMatchupAndBookie');
+  $group->get('/proptemplate', \AdminController::class . ':addNewPropTemplate');
+  $group->get('/proptemplates', \AdminController::class . ':viewPropTemplates');
+  $group->get('/resetchangenums', \AdminController::class . ':resetChangeNums');
+  $group->get('/testMail', \AdminController::class . ':testMail');
+  $group->get('/logs[/{logfile}]', \AdminController::class . ':viewLatestLog');
+  $group->get('/parserlogs[/{bookie_name}]', \AdminController::class . ':viewParserLogs');
+  $group->get('/alerts', \AdminController::class . ':viewAlerts');
+  $group->get('/matchups/{id}', \AdminController::class . ':viewMatchup');
+  $group->get('/propcorrelation', \AdminController::class . ':createPropCorrelation');
+  $group->get('/odds', \AdminController::class . ':oddsOverview');
+  $group->get('/flagged', \AdminController::class . ':viewFlaggedOdds');
+  $group->get('/parser_status', \AdminController::class . ':viewParserStatus');
+  $group->get('/changeauditlog', \AdminController::class . ':viewChangeAuditLog');
+  
+})->add(new AuthMiddleware());
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<title>Admin</title>
-<script src="../js/jquery-1.11.3.min.js" language="JavaScript" type="text/javascript"></script>
-<script src="js/jquery.autocomplete.min.js" language="JavaScript" type="text/javascript"></script>
-<script src="js/admin-main.js" language="JavaScript" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="css/stylesheets.php" />
-</head>
-<body>
-<div id="adminMenu">
-<a href="index.php" style="color: #a30000; text-decoration: none;">
-<h1 style="">Admin</h1></a>
 
-<a href="index.php">Main</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=viewManualActions">Schedule</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=addNewEventForm">New Event</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=addNewFightForm">New Fight</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=eventsOverview">Events overview</a> <a href="?p=eventsOverview&amp;show=all">(all)</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=addFighterAltName">Altnames</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=addOddsManually">New odds</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=addNewPropTemplate">New prop template</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=viewPropTemplates">View prop templates</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=resetChangeNum">Reset changenum</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=testMail">Test mail</a>&nbsp;&nbsp;&nbsp;
-<a href="index.php?p=viewLatestLog&log=latest">View latest log</a> <a href="index.php?p=viewLatestLog">(all)</a><br /><br />
-</div>
-<div class="contentWindow">
-<?php
+//API Routes
+$app->group('/api', function (RouteCollectorProxy $group) {
 
-if (isset($_GET['p']) && $_GET['p'] != '' && preg_match('/[a-zA-Z0-9]*/', $_GET['p']))
-{
+  $group->post('/matchups', \AdminAPIController::class . ':createMatchup');
+  $group->put('/matchups/{id}', \AdminAPIController::class . ':updateMatchup');
+  $group->delete('/matchups/{id}', \AdminAPIController::class . ':deleteMatchup');
 
-	include_once('app/front/cnadm/pages/' . $_GET['p'] . '.php');
-}
-else
-{
-	//Show default
-	include_once('app/front/cnadm/pages/main.php');
-}
+  $group->post('/events', \AdminAPIController::class . ':createEvent');
+  $group->put('/events/{id}', \AdminAPIController::class . ':updateEvent');
+  $group->delete('/events/{id}', \AdminAPIController::class . ':deleteEvent');
 
-/*if (isset($_GET['message']) && $_GET['message'] != '')
-{
-	echo '<script language="Javascript">alert ("' . $_GET['message'] . '")</script>';
-}*/
+  $group->put('/fighters/{id}', \AdminAPIController::class . ':updateFighter');
 
-?>
-</div>
-</body>
-</html>
+  $group->post('/resetchangenums', \AdminAPIController::class . ':resetChangeNum');
+  $group->post('/clearunmatched', \AdminAPIController::class . ':clearUnmatched');
+  $group->post('/proptemplates', \AdminAPIController::class . ':createPropTemplate');
+  $group->post('/propcorrelation', \AdminAPIController::class . ':createPropCorrelation');
+  $group->delete('/manualactions/{id}', \AdminAPIController::class . ':deleteManualAction');
+
+  $group->delete('/odds', \AdminAPIController::class . ':deleteOdds');
+});
+$app->run();
