a8509a51bdf3f37adcec1ad4f73b346ae7cd66ad
cnordvaller <cn@capapps.se>
2021-04-22 19:14:28 +0200

    Excluding SUG events from BetOnline

--- bfo/app/parsers/cron/cron.BetOnline.php
+++ bfo/app/parsers/cron/cron.BetOnline.php
@@ -120,7 +120,8 @@ class ParserJob
             //Ignore certain events (e.g. non-MMA)
             if (isset($matchup->scheduleText) 
                 && (substr(trim((string) $matchup->scheduleText),0,4) == 'BKFC'
-                || substr(trim((string) $matchup->scheduleText),0,9) == 'Fight2Win'))
+                || substr(trim((string) $matchup->scheduleText),0,9) == 'Fight2Win'
+                || substr(trim((string) $matchup->scheduleText),0,3) == 'SUG'))
             {
                 $this->logger->info('Skipping matchup for event ' . $matchup->scheduleText);
             }
