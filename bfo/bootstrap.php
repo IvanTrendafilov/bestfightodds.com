d5f9aeb8efdd6edcb06d27fdff3bd9bbceef7820
cnordvaller <cn@capapps.se>
2021-05-02 19:48:18 +0200

    Bootstrap file and autoload

--- bfo/bootstrap.php
+++ bfo/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 
+require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '../config/inc.config.php';
 
-require_once 'vendor/autoload.php';
-$path = 'shared/';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-$path = 'bfo/';
+$path = __DIR__ . '/../shared/';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
-require_once 'config/inc.config.php';
-require_once 'lib/bfocore/general/inc.GlobalTypes.php';
+/*require_once 'config/inc.config.php';
+require_once 'lib/bfocore/general/inc.GlobalTypes.php';*/
