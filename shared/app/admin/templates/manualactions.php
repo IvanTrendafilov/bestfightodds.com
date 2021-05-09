5f1b0b986d59304f4710ad8777ef3f139919f9a1
cnordvaller <cn@capapps.se>
2021-05-09 11:44:58 +0200

    Accept all create fix

--- shared/app/admin/templates/manualactions.php
+++ shared/app/admin/templates/manualactions.php
@@ -1,6 +1,6 @@
 <?php $this->layout('base/layout', ['title' => 'Admin']) ?>
 
-Manual actions: <button class="btn btn-primary" onclick="$('input[onclick^=\'maAdd\']').click();">Accept all <b>Create</b> actions below</button>
+Manual actions: <button class="btn btn-primary" onclick="$('button[onclick^=\'maAdd\']').click();">Accept all <b>Create</b> actions below</button>
 <br /><br />
 
 <?php if ($actions != null && count($actions) > 0) : ?>
