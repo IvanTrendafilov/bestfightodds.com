df8480ff0dc66693eb5eefb36946a9a66f8639f4
cnordvaller <cn@capapps.se>
2021-05-13 22:16:07 +0200

    Cleanup

--- shared/src/BFO/General/TwitterHandler.php
+++ shared/src/BFO/General/TwitterHandler.php
@@ -15,95 +15,94 @@ class TwitterHandler
 {
     public static function tweetNewMatchups()
     {
-        $iTwits = 0;
-        $aFights = TwitterDB::getUntweetedMatchups();
+        $tweet_counter = 0;
+        $matchups = TwitterDB::getUntweetedMatchups();
 
         // Fights are mainly grouped based on event, however main events are always seperated into their own group
-        $aGroups = array();
+        $tweet_groups = array();
 
-        foreach ($aFights as $oFight) {
+        foreach ($matchups as $matchup) {
             //We don't group if this is a UFC matchup so fetch the event
-            $oEvent = EventHandler::getEvent($oFight->getEventID());
+            $event = EventHandler::getEvent($matchup->getEventID());
 
-            if (!TWITTER_GROUP_MATCHUPS || $oFight->isMainEvent() || substr(strtoupper($oEvent->getName()), 0, 3) == 'UFC') {
-                $aGroups[$oFight->getEventID() . $oFight->getID()]['matchups'][] = $oFight;
-                $aGroups[$oFight->getEventID() . $oFight->getID()]['event_id'] = $oFight->getEventID();
+            if (!TWITTER_GROUP_MATCHUPS || $matchup->isMainEvent() || substr(strtoupper($event->getName()), 0, 3) == 'UFC') {
+                $tweet_groups[$matchup->getEventID() . $matchup->getID()]['matchups'][] = $matchup;
+                $tweet_groups[$matchup->getEventID() . $matchup->getID()]['event_id'] = $matchup->getEventID();
             } else {
-                $aGroups[$oFight->getEventID()]['matchups'][] = $oFight;
-                $aGroups[$oFight->getEventID()]['event_id'] = $oFight->getEventID();
+                $tweet_groups[$matchup->getEventID()]['matchups'][] = $matchup;
+                $tweet_groups[$matchup->getEventID()]['event_id'] = $matchup->getEventID();
             }
         }
 
         $tweeter = new Tweeter(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OATUH_TOKEN_SECRET);
         $tweeter->setDebugMode(TWITTER_DEV_MODE);
 
-        foreach ($aGroups as $aGroup) {
-            $oEvent = EventHandler::getEvent($aGroup['event_id']);
+        foreach ($tweet_groups as $group) {
+            $event = EventHandler::getEvent($group['event_id']);
 
-            $bUpdateSuccess = false;
-            $sTwitText = '';
-            if (count($aGroup['matchups']) > 1) {
+            $text_to_tweet = '';
+            if (count($group['matchups']) > 1) {
                 //Multiple fights for the same event needs to be tweeted
-                $sTwitText = str_replace(
-                    array('<T1>','<T2>'),
-                    array($aGroup['matchups'][0]->getFighterAsString(1), $aGroup['matchups'][0]->getFighterAsString(2)),
+                $text_to_tweet = str_replace(
+                    array('<T1>', '<T2>'),
+                    array($group['matchups'][0]->getFighterAsString(1), $group['matchups'][0]->getFighterAsString(2)),
                     TWITTER_TEMPLATE_MULTI
                 );
-            } elseif (count($aGroup['matchups']) == 1) {
+            } elseif (count($group['matchups']) == 1) {
                 //Only one fight for the event needs to be tweeted
-                $oFightOdds = OddsHandler::getOpeningOddsForMatchup($aGroup['matchups'][0]->getID());
-                if ($oFightOdds != null) {
-                    $sTwitText = str_replace(
-                        array('<T1>','<T2>','<T1O>','<T2O>'),
-                        array($aGroup['matchups'][0]->getFighterAsString(1), $aGroup['matchups'][0]->getFighterAsString(2), $oFightOdds->getFighterOddsAsString(1), $oFightOdds->getFighterOddsAsString(2)),
+                $odds = OddsHandler::getOpeningOddsForMatchup($group['matchups'][0]->getID());
+                if ($odds != null) {
+                    $text_to_tweet = str_replace(
+                        array('<T1>', '<T2>', '<T1O>', '<T2O>'),
+                        array($group['matchups'][0]->getFighterAsString(1), $group['matchups'][0]->getFighterAsString(2), $odds->getFighterOddsAsString(1), $odds->getFighterOddsAsString(2)),
                         TWITTER_TEMPLATE_SINGLE
                     );
                 }
             }
 
-            $iTweetLength = strlen($sTwitText) + 13; //12 is to reach the URL character limit (23) with <EVENT_URL> already counted for
+            $tweet_length = strlen($text_to_tweet) + 13; //12 is to reach the URL character limit (23) with <EVENT_URL> already counted for
             //Substitute event URL (doing this here because we need to count characters without URL prior to this)
-            $sTwitText = str_replace('<EVENT_URL>', $oEvent->getEventAsLinkString(), $sTwitText);
+            $text_to_tweet = str_replace('<EVENT_URL>', $event->getEventAsLinkString(), $text_to_tweet);
 
             //Depending on how many characters we have left, add either full event name or the shortened version
-            if (strlen($sTwitText) + strlen($oEvent->getName()) <= 280) {
-                $sTwitText = str_replace('<E>', $oEvent->getName(), $sTwitText);
+            if (strlen($text_to_tweet) + strlen($event->getName()) <= 280) {
+                $text_to_tweet = str_replace('<E>', $event->getName(), $text_to_tweet);
             } else {
-                $sTwitText = str_replace('<E>', $oEvent->getShortName(), $sTwitText);
+                $text_to_tweet = str_replace('<E>', $event->getShortName(), $text_to_tweet);
             }
 
             //Add dynamic hashtags if less than 280 chars and if event name format allows for it
-            $aMatches = null;
-            $sRegExp = preg_match("/^([a-zA-Z]{2,8}\s[0-9]{1,5}):/", $oEvent->getName(), $aMatches);
-            if ($aMatches != null && isset($aMatches[1])) {
-                $sHashtag = ' #' . strtolower(str_replace(' ', '', $aMatches[1]));
-                if (strlen($sHashtag) + $iTweetLength <= 280) {
-                    $sTwitText .= $sHashtag;
+            $regexp_matches = null;
+            $sRegExp = preg_match("/^([a-zA-Z]{2,8}\s[0-9]{1,5}):/", $event->getName(), $regexp_matches);
+            if ($regexp_matches != null && isset($regexp_matches[1])) {
+                $hashtag = ' #' . strtolower(str_replace(' ', '', $regexp_matches[1]));
+                if (strlen($hashtag) + $tweet_length <= 280) {
+                    $text_to_tweet .= $hashtag;
                 }
             }
 
             //Add fighter twitter handles if we are only tweeting one matchup (and if they exist)
-            if (count($aGroup['matchups']) == 1) {
+            if (count($group['matchups']) == 1) {
                 for ($x = 1; $x <= 2; $x++) {
-                    $handle = self::getTwitterHandle($aGroup['matchups'][0]->getFighterID($x));
+                    $handle = self::getTwitterHandle($group['matchups'][0]->getFighterID($x));
                     if ($handle && $handle != '') {
-                        $sTwitText .= ' @' . $handle;
+                        $text_to_tweet .= ' @' . $handle;
                     }
                 }
             }
 
             //Trim to 280 if we made any mistakes along the way
-            $sTwitText = substr($sTwitText, 0, 280);
+            $text_to_tweet = substr($text_to_tweet, 0, 280);
 
-            if ($sTwitText != '' && $tweeter->updateStatus($sTwitText)) {
-                foreach ($aGroup['matchups'] as $oEventFight) {
-                    TwitterDB::saveFightAsTweeted($oEventFight->getID());
+            if ($text_to_tweet != '' && $tweeter->updateStatus($text_to_tweet)) {
+                foreach ($group['matchups'] as $matchup_tweeted) {
+                    TwitterDB::saveFightAsTweeted($matchup_tweeted->getID());
                 }
-                $iTwits++;
+                $tweet_counter++;
             }
         }
 
-        return array('pre_untweeted_fights' => count($aFights), 'pre_untweeted_events' => count($aGroups), 'post_tweeted' => $iTwits);
+        return array('pre_untweeted_fights' => count($matchups), 'pre_untweeted_events' => count($tweet_groups), 'post_tweeted' => $tweet_counter);
     }
 
     public static function addTwitterHandle($team_id, $handle)
