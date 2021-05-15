ba98d2164e21482f5f9c6517c2796af3cd43beba
cnordvaller <cn@capapps.se>
2021-05-15 17:27:39 +0200

    Parser status fix

--- shared/app/admin/templates/parser_status.php
+++ shared/app/admin/templates/parser_status.php
@@ -2,20 +2,22 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                Parsed new matchups/pros in last 24 hours: 
+                Parsed new matchups/pros in last 24 hours:
                 <?php foreach ($runstatus as $runstatus_entry) : ?>
                     <a href="/cnadm/parserlogs/<?= strtolower($runstatus_entry['name']) ?>"><span class="badge <?= ($runstatus_entry['average_matched'] <= 0 ? 'bg-danger' : 'bg-success') ?>"><?= $runstatus_entry['name'] ?></span></a>
                 <?php endforeach ?><br>
-                Has finished in last 5 minutes: 
+                Has finished in last 5 minutes:
                 <?php foreach ($lastfinishes as $bookie => $status) : ?>
-                    <a href="/cnadm/parserlogs/<?=strtolower($bookie)?>"><span class="badge <?= ($status == false ? 'bg-danger' : 'bg-success') ?>"><?=$bookie?></span></a>
+                    <a href="/cnadm/parserlogs/<?= strtolower($bookie) ?>"><span class="badge <?= ($status == false ? 'bg-danger' : 'bg-success') ?>"><?= $bookie ?></span></a>
                 <?php endforeach ?><br>
-                Odds job finished in last 5 minutes: 
-                <?php if ($oddsjob_finished): ?>
-                    <span class="badge bg-success">Yes</span>
-                <?php else: ?>
-                    <span class="badge bg-danger">No</span>
-                <?php endif ?>
+                Odds job finished in last 5 minutes:
+                <a href="/cnadm/logs/latest">
+                    <?php if ($oddsjob_finished) : ?>
+                </a><span class="badge bg-success">Yes</span>
+            <?php else : ?>
+                <span class="badge bg-danger">No</span>
+            <?php endif ?>
+            </a>
             </div>
         </div>
     </div>
