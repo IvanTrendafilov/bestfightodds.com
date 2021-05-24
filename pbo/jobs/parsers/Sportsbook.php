66714417cfbe0dfad65866e5879a4c2b6a8ac83f
cnordvaller <cn@capapps.se>
2021-05-24 19:24:59 +0200

    Sportsbook PBO fix

--- pbo/jobs/parsers/Sportsbook.php
+++ pbo/jobs/parsers/Sportsbook.php
@@ -134,7 +134,7 @@ class ParserJob
         }
 
         //Declare authorative run if we fill the criteria
-        if (!$failed_once && count($parsed_sport->getParsedMatchups()) >= 10)
+        if (!$failed_once && count($parsed_sport->getParsedMatchups()) >= 3)
         {
             $this->full_run = true;
             $this->logger->info("Declared full run");
