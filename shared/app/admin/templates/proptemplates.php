56226fee9331f6c3d4407a374dd79ddff4ea3a5d
cnordvaller <cn@capapps.se>
2021-05-30 22:24:13 +0200

    Minor fixes

--- shared/app/admin/templates/proptemplates.php
+++ shared/app/admin/templates/proptemplates.php
@@ -6,33 +6,35 @@
         <div class="card-header">
             <h5 class="card-title"><?= $this->e($bookie['bookie']->getName()) ?></h5>
         </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Template</th>
-                    <th>Proptype ID</th>
-                    <th>Field Type</th>
-                    <th>Last used</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-
-            <tbody class="bg-white">
-                <?php foreach ($bookie['templates'] as $template) : ?>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?= $template->getID() ?></td>
-                        <td>
-                            <div><?= str_replace(' / ', '</div><div>', $template->toString()) ?></div>
-                        </td>
-                        <td><?= $template->getPropTypeID() ?></td>
-                        <td>e.g: <?= $template->getFieldsTypeAsExample() ?></td>
-                        <td><?= $template->getLastUsedDate() ?></td>
-                        <td><button data-li="<?= $template->getID() ?>">Delete</button></td>
+                        <th>ID</th>
+                        <th>Template</th>
+                        <th>Proptype ID</th>
+                        <th>Field Type</th>
+                        <th>Last used</th>
+                        <th>Action</th>
                     </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody class="bg-white">
+                    <?php foreach ($bookie['templates'] as $template) : ?>
+                        <tr>
+                            <td><?= $template->getID() ?></td>
+                            <td>
+                                <div><?= str_replace(' / ', '</div><div>', $template->toString()) ?></div>
+                            </td>
+                            <td><?= $template->getPropTypeID() ?></td>
+                            <td>e.g: <?= $template->getFieldsTypeAsExample() ?></td>
+                            <td><?= $template->getLastUsedDate() ?></td>
+                            <td><button class="btn btn-danger" data-li="<?= $template->getID() ?>">Delete</button></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
 <?php endforeach ?>
