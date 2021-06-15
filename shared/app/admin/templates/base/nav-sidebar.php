f2e6dc657e0a0b26662ec21391efb8d042aa8718
cnordvaller <cn@capapps.se>
2021-06-15 10:29:59 +0200

    Admin sidebar update

--- shared/app/admin/templates/base/nav-sidebar.php
+++ shared/app/admin/templates/base/nav-sidebar.php
@@ -56,13 +56,13 @@
 				</a>
 			</li>
 
-			<li class="sidebar-item <?= $current_page == 'logs' ? 'active' : '' ?>">
+			<li class="sidebar-item">
 				<a class="sidebar-link" href="/cnadm/logs/latest">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book align-middle"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg> <span class="align-middle">Odds job log</span>
 				</a>
 			</li>
 
-			<li class="sidebar-item <?= $current_page == 'changeaudit' ? 'active' : '' ?>">
+			<li class="sidebar-item">
 				<a class="sidebar-link" href="/cnadm/log/changeaudit">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book align-middle"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg> <span class="align-middle">Change audit log</span>
 				</a>
