9a4afee2686faf5053df1c5d3b21bea202e992ce
cnordvaller <cn@capapps.se>
2021-06-18 13:46:45 +0200

    Added bookie order integritry check

--- pbo/app/front/js/bfo_settings.js
+++ pbo/app/front/js/bfo_settings.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    document.getElementById('bookieHideSelector').addEventListener("click", function(e) {
+    document.getElementById('bookieHideSelector').addEventListener("click", function (e) {
         showSettingsWindow();
     }, false);
 
-    document.getElementById('bookieResetDefault').addEventListener("click", function(e) {
+    document.getElementById('bookieResetDefault').addEventListener("click", function (e) {
         localStorage.removeItem("pbo_bookie_order");
         location.reload();
         //Reset to defaults
@@ -11,19 +11,17 @@ $(document).ready(function () {
 
     var el = document.getElementById('bookie-order-items');
     var sortable = new Sortable(el, {
-        onUpdate: function(item) {saveBookieSettings(el)},
+        onUpdate: function (item) { saveBookieSettings(el) },
         filter: ".inp-checkbox",
         animation: 150
-        }
+    }
     );
 });
 function updateBookieColumns() {
     //Only apply to pages that contain table-scroller (an odds table)
-    if (document.querySelector('.table-scroller'))
-    {
+    if (document.querySelector('.table-scroller')) {
         var current_settings = localStorage.getItem("pbo_bookie_order");
-        if (current_settings !== null) 
-        {
+        if (current_settings !== null) {
             current_settings = JSON.parse(current_settings)
             //Re-order columns        
             remapped = [];
@@ -31,26 +29,38 @@ function updateBookieColumns() {
                 remapped[item[1].order] = item[0];
             });
             remapped.reverse();
+
+            //Validate integrity of stored order. It will not be valid if any bookie has been removed or added
+            var abort = false;
             remapped.forEach(function (item) {
-                var cur_index = document.querySelectorAll('.table-scroller')[0].querySelector('th[data-b="' + item + '"]').cellIndex;
-                
-                document.querySelectorAll('.table-scroller table').forEach(function (table) {
-                    for (var i = 0, row; row = table.rows[i]; i++) {
-                        row.cells[1].insertAdjacentElement('afterend', row.cells[cur_index]);
-                        //Check if this bookie should be disabled as well
-                        if (current_settings[item].enabled == false) {
-                            row.cells[2].style.display = "none";
-                        }
-                        else {
-                            row.cells[2].setAttribute("style", "");
-                        }
-                    }
-                });
+                if (document.querySelectorAll('.table-scroller')[0].querySelector('th[data-b="' + item + '"]') === null) {
+                    abort = true;
+                }
             });
+
+            if (!abort) {
+                remapped.forEach(function (item) {
+                    var cur_index = document.querySelectorAll('.table-scroller')[0].querySelector('th[data-b="' + item + '"]').cellIndex;
+
+                    document.querySelectorAll('.table-scroller table').forEach(function (table) {
+                        for (var i = 0, row; row = table.rows[i]; i++) {
+                            row.cells[1].insertAdjacentElement('afterend', row.cells[cur_index]);
+                            //Check if this bookie should be disabled as well
+                            if (current_settings[item].enabled == false) {
+                                row.cells[2].style.display = "none";
+                            }
+                            else {
+                                row.cells[2].setAttribute("style", "");
+                            }
+                        }
+                    });
+                });
+            } else {
+                localStorage.removeItem("pbo_bookie_order");
+            }
         }
     }
-    else
-    {
+    else {
         //Hide bookie selector in options since we cannot properly populate the values in the selector due to lack of odds tables
         document.getElementById('bookieHideSelector').parentNode.style.display = 'none';
     }
@@ -77,11 +87,10 @@ function showSettingsWindow() {
     el.innerHTML = '';
 
     var current_settings = localStorage.getItem("pbo_bookie_order");
-    if (current_settings) 
-    {
+    if (current_settings) {
         current_settings = JSON.parse(current_settings)
     }
-    
+
     document.querySelectorAll('.table-scroller')[0].querySelectorAll('th[data-b]').forEach(function (item) {
         bookies[item.cellIndex] = item.dataset.b;
         var checked = 'checked';
@@ -92,7 +101,7 @@ function showSettingsWindow() {
     });
 
     el.querySelectorAll('.inp-checkbox').forEach(function (item) {
-        item.addEventListener("change", function(e) {
+        item.addEventListener("change", function (e) {
             saveBookieSettings(document.querySelector('#bookie-order-items'));
         }, false);
     });
