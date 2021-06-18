01b7ca06e3ce702176457e2482ca96255f510744
cnordvaller <cn@capapps.se>
2021-06-18 16:01:58 +0200

    Updated terms

--- pbo/app/front/templates/terms.php
+++ pbo/app/front/templates/terms.php
@@ -4,12 +4,12 @@
     <div id="page-container">
         <div id="page-inner-wrapper">
             <div id="page-content">
-            <h1>Terms of service</h1>
-            <p>Although the service is designed to give accurate information it is provided
-                on an "as is" and "as available" basis. We expressely disclaim any and all
-                warranties, express or implied, including any warranties as to the
-                availability, accuracy or content of the information.</p>
-                            <p style="padding: 10px 0;">For any questions regarding the site and its content, please contact us at <a href="mailto:info@proboxingodds.com" style="color: #222;">info@proboxingodds.com</a></p>
+                <h1>Terms of service</h1>
+                <p>Although the service is designed to give accurate information it is provided
+                    on an "as is" and "as available" basis. We expressely disclaim any and all
+                    warranties, express or implied, including any warranties as to the
+                    availability, accuracy or content of the information.</p>
+                <p style="padding: 10px 0;">For any questions regarding the site and its content, please contact us at <a href="mailto:info@proboxingodds.com" style="color: #222;">info@proboxingodds.com</a></p>
             </div>
         </div>
     </div>
