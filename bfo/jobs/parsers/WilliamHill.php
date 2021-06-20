e9e8ea9f40003347ee293098cb50f1738b375a48
cnordvaller <cn@capapps.se>
2021-06-20 12:00:38 +0200

    WH prop date check fix

--- bfo/jobs/parsers/WilliamHill.php
+++ bfo/jobs/parsers/WilliamHill.php
@@ -158,6 +158,11 @@ class ParserJob extends ParserJobBase
 
     private function parseProp($matchup, $market): void
     {
+        $date_obj = new DateTime((string) $matchup->startTime);
+        if ($date_obj < new DateTime()) {
+            return;
+        }
+        
         if (count($market->selections) == 2) {
             //Two way prop
             if (
@@ -166,11 +171,6 @@ class ParserJob extends ParserJobBase
                 !empty($market->selections[0]->price?->a) &&
                 !empty($market->selections[1]->price?->a)
             ) {
-                $date_obj = new DateTime((string) $matchup->startTime);
-                if ($date_obj < new DateTime()) {
-                    return;
-                }
-
                 $parsed_prop = new ParsedProp(
                     $matchup->name . ' :: ' . $market->name . ' : ' . $market->selections[0]->name,
                     $matchup->name . ' :: ' . $market->name . ' : ' . $market->selections[1]->name,
