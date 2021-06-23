7dd3ea9740f133eebe668bf9472b645dde5107af
cnordvaller <cn@capapps.se>
2021-06-23 21:43:16 +0200

    Unibet over under fix

--- pbo/jobs/parsers/Unibet.php
+++ pbo/jobs/parsers/Unibet.php
@@ -163,8 +163,8 @@ class ParserJob extends ParserJobBase
                 $label2 = ParseTools::convertCommaNameToFullName($betoffer->outcomes[1]->label);
 
                 $parsed_prop = new ParsedProp(
-                    $event_name_adjusted . ' :: ' . (isset($betoffer->outcomes[0]->participant) ? ParseTools::convertCommaNameToFullName($betoffer->outcomes[0]->participant) . ' ' : '') . $betoffer->criterion->label . ' : ' . $label1,
-                    $event_name_adjusted . ' :: ' . (isset($betoffer->outcomes[1]->participant) ? ParseTools::convertCommaNameToFullName($betoffer->outcomes[1]->participant) . ' ' : '') . $betoffer->criterion->label . ' : ' . $label2,
+                    $event_name_adjusted . ' :: ' . (isset($betoffer->outcomes[0]->participant) ? ParseTools::convertCommaNameToFullName($betoffer->outcomes[0]->participant) . ' ' : '') . $betoffer->criterion->label . (isset($betoffer->outcomes[0]->line) ? ' ' . $betoffer->outcomes[0]->line : '') . ' : ' . $label1,
+                    $event_name_adjusted . ' :: ' . (isset($betoffer->outcomes[1]->participant) ? ParseTools::convertCommaNameToFullName($betoffer->outcomes[1]->participant) . ' ' : '') . $betoffer->criterion->label . (isset($betoffer->outcomes[1]->line) ? ' ' . $betoffer->outcomes[1]->line : '') . ' : ' . $label2,
                     $betoffer->outcomes[0]->oddsAmerican,
                     $betoffer->outcomes[1]->oddsAmerican
                 );
@@ -213,7 +213,7 @@ class ParserJob extends ParserJobBase
                     }
 
                     $parsed_prop = new ParsedProp(
-                        $event_name_adjusted . ' :: ' . (isset($outcome->participant) ? ParseTools::convertCommaNameToFullName($outcome->participant) . ' ' : '') . $betoffer->criterion->label . ' : ' . $new_label,
+                        $event_name_adjusted . ' :: ' . (isset($outcome->participant) ? ParseTools::convertCommaNameToFullName($outcome->participant) . ' ' : '') . $betoffer->criterion->label . (isset($outcome->line) ? ' ' . $outcome->line : '') . ' : ' . $new_label,
                         '',
                         $outcome->oddsAmerican,
                         '-99999'
